21891fc008a5f84ec2dd59d339baa901b54b4397
David Bickram <david.sony2003@gmail.com>
2026-09-12 21:53:00 -0400

    Zephyr workqueue blinky

--- firmware/wearable/src/main.c
+++ firmware/wearable/src/main.c
@@ -1,13 +1,30 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
 
-#define SLEEP_TIME_MS 5000
+LOG_MODULE_REGISTER(wearable, LOG_LEVEL_INF);
 
 #define LED0_NODE DT_ALIAS(led0)
 
 static const struct gpio_dt_spec led =
     GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+static void blink_work_handler(struct k_work *work);
+
+K_WORK_DELAYABLE_DEFINE(blink_work, blink_work_handler);
+
+static void blink_work_handler(struct k_work *work)
+{
+    ARG_UNUSED(work);
+
+    gpio_pin_toggle_dt(&led);
+
+    LOG_INF("Uptime: %lld ms",
+            (long long)k_uptime_get());
+
+    k_work_reschedule(&blink_work, K_MSEC(500));
+}
 
 int main(void)
 {
@@ -17,10 +34,11 @@ int main(void)
 
     gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
 
-    while (1) {
-        gpio_pin_toggle_dt(&led);
-        k_msleep(SLEEP_TIME_MS);
-    }
+    LOG_INF("Hand Controlled Smart Light starting");
+
+    k_work_schedule(&blink_work, K_NO_WAIT);
+
+    k_sleep(K_FOREVER);
 
     return 0;
 }
